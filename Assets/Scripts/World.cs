8fd1f328f759d0cb152ac4ba56391004cb73bd4d
Alberto Boffi <alberto2.boffi@mail.polimi.it>
2023-11-19 11:43:28 +0100

    feat: add World class

---
+++ Assets/Scripts/World.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class World{
+    
+    // SPEEDS ARE EXPRESSED IN [km/h]
+
+    // Speed of light
+    private float c;
+
+    public World(){
+
+        this.c = 40.0f;
+
+    }
+
+}
