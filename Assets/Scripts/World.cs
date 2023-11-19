c3acbfeb1345aa5b44656b007213b1210f795897
Alberto Boffi <alberto2.boffi@mail.polimi.it>
2023-11-19 11:51:24 +0100

    feat: implement velocity composition

--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -13,4 +13,14 @@ public class World{
 
     }
 
+    // Velocity composition law
+    public float getRelativeSpeed(float v_a, float v_b){
+        
+        float gal_term = v_b - v_a;
+        float ein_term = 1 - (v_a * v_b) / Mathf.Pow(c, 2);
+        
+        return gal_term / ein_term;
+
+    }
+
 }
