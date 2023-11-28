59bf5819283b4e182c150e516ca1c342a15c6825
Alberto Boffi <alberto2.boffi@mail.polimi.it>
2023-11-28 07:46:24 +0100

    feat: implement cars contraction

--- Assets/Scripts/ObserverMonoBehaviour.cs
+++ Assets/Scripts/ObserverMonoBehaviour.cs
@@ -11,11 +11,37 @@ public class ObserverMonoBehavior : MonoBehaviour{
     private float speed;
     private World world;
 
+    private void contractCar(CarMonoBehaviour car){
+
+        float car_speed = this.world.getRelativeSpeed(this.speed, car.getSpeed());
+        float car_length = car.getLength();
+
+        var contr_param = this.world.contractSpace(car_speed, car_length);
+
+        car.setScale(contr_param.scale);
+        car.setShift(contr_param.shift);
+
+    }
+
     void Start(){
         
+        this.speed = 0;
+        this.world = new World();
+
+        CarMonoBehaviour car;
+
+        foreach (GameObject car_obj in this.carsObj){
+
+            car = car_obj.GetComponent<CarMonoBehaviour>();
+            this.contractCar(car);
+
+        }
+
     }
 
     void Update(){
+
+        // transform.Translate(this.velocity * Time.deltaTime, Space.World);
         
     }
 }
