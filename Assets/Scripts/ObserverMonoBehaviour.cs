737079748e733b15583da813d4f6b7483ded4997
Alberto Boffi <alberto2.boffi@mail.polimi.it>
2023-11-28 07:36:18 +0100

    feat: create observer class

---
+++ Assets/Scripts/ObserverMonoBehaviour.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObserverMonoBehavior : MonoBehaviour{
+
+    // SPEEDS ARE EXPRESSED IN [km/h]
+    
+    public List<GameObject> carsObj;
+
+    private float speed;
+    private World world;
+
+    void Start(){
+        
+    }
+
+    void Update(){
+        
+    }
+}
