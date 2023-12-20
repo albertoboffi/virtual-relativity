2bfe8f19b49c109de733f99f4b2a6e2c3ef88d56
Alberto Boffi <alberto2.boffi@mail.polimi.it>
2023-12-20 21:42:28 +0100

    feat: implement time dilation

--- Assets/Scripts/TimeDilation_Sea/BoatMonoBehaviour.cs
+++ Assets/Scripts/TimeDilation_Sea/BoatMonoBehaviour.cs
@@ -6,10 +6,17 @@ public class BoatMonoBehaviour : MonoBehaviour{
     
     // SPEEDS ARE EXPRESSED IN [km/h]
 
+    public GameObject seaObj;
+    public List<GameObject> dolphins;
+
+    private World world;
+
     private float speed; // speed relative to the world
     private Vector3 direction; // direction of motion
 
     public BoatMonoBehaviour(){
+
+        this.world = new World();
 
         this.speed = 20.0f;
         this.direction = new Vector3(1, 0, 0); // the boar move along the x-direction in the world space
@@ -17,7 +24,29 @@ public class BoatMonoBehaviour : MonoBehaviour{
     }
 
     void Start(){
+
+        DolphinMonoBehaviour dolphin;
+        SeaMonoBehaviour sea = this.seaObj.GetComponent<SeaMonoBehaviour>();
         
+        float boat_speed = this.world.getRelativeSpeed(
+        
+            this.speed,
+            sea.getSpeed()
+        
+        );
+
+        var dilation_factor = this.world.dilateTime(boat_speed);
+
+        Debug.Log(dilation_factor);
+
+        foreach (GameObject dolphin_obj in this.dolphins){
+
+            dolphin = dolphin_obj.GetComponent<DolphinMonoBehaviour>();
+            dolphin.setSlowMotion(dilation_factor);
+
+        }
+
+
     }
 
     void Update(){
