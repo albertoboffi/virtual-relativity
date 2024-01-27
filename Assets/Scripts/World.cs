de442d8df7114398d5947370e4e28084cef570fb
Alberto Boffi <alberto2.boffi@mail.polimi.it>
2024-01-27 14:36:31 +0100

    feat: equip the world with method to handle obejcts

--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -1,19 +1,48 @@
 using UnityEngine;
+using System;
 
 public class World{
     
     // SPEEDS ARE EXPRESSED IN [km/h]
 
-    // Speed of light
-    private float c;
+    private float c; // speed of light
+
+    Dictionary<GameObject, Dictionary<string, float>> objects; // objects inside the world
 
     public World(float c){
 
         this.c = c;
+        this.objects = new Dictionary<GameObject, Dictionary<string, float>>();
+
+    }
+
+    // Adds object to the world -> Axioms 2 - 4
+
+    public void addObject(GameObject obj){
+
+        this.objects.Add(
+            obj,
+            new Dictionary<string, float>
+        );
+
+    }
+
+    // Set the speed of an object inside the world -> Axiom 3
+
+    public void setSpeed(GameObject obj, float v){
+
+        if (Math.Abs(v) >= Math.Abs(this.c)){ // |v| < |c| -> Axiom 5
+
+            throw new ArgumentException("Objects must be slower than light");
+
+        }
+
+        this.objects[obj].Add("v", v);
 
     }
 
     // Velocity composition law
+
     public float getRelativeSpeed(float v_a, float v_b){
         
         float gal_term = v_b - v_a;
@@ -24,6 +53,7 @@ public class World{
     }
 
     // Space contraction
+
     public (float scale, float shift) contractSpace(float v, float length){
 
         float scale = Mathf.Sqrt(1 - Mathf.Pow(v, 2) / Mathf.Pow(this.c, 2));
@@ -34,6 +64,7 @@ public class World{
     }
 
     // Time dilation
+
     public float dilateTime(float v){
 
         float scale = Mathf.Sqrt(1 - Mathf.Pow(v, 2) / Mathf.Pow(this.c, 2));
