25022fd9664deb1e6b298a29b66a8d13518acd19
Alberto Boffi <alberto2.boffi@mail.polimi.it>
2024-01-28 10:04:49 +0100

    feat: change ObserverMonoBehaviour to match the new standard

--- Assets/Scripts/space-contraction/nyc/ObserverMonoBehaviour.cs
+++ Assets/Scripts/space-contraction/nyc/ObserverMonoBehaviour.cs
@@ -4,62 +4,25 @@ using UnityEngine;
 
 public class ObserverMonoBehavior : MonoBehaviour{
 
-    /*
-
-    // SPEEDS ARE EXPRESSED IN [km/h]
-    
-    public List<GameObject> carsObj;
-
-    private float speed;
-    private World world;
-
-    private void contractCar(CarMonoBehaviour car){
-
-        float car_length = car.getLength();
-
-        float car_speed = this.world.getRelativeSpeed(
-        
-            this.speed,
-            car.getSpeed()
-        
-        );
-
-        var contr_param = this.world.contractSpace(
-        
-            car_speed,
-            car_length
-            
-        );
-
-        car.setScale(contr_param.scale);
-        car.setShift(contr_param.shift);
-
-    }
-
-    public ObserverMonoBehavior(){
-
-        this.speed = 0;
-        this.world = new World(45.0f);
-
-    }
+    public List<GameObject> cars;
 
     void Start(){
 
+        // CONSTRUCTOR
+
+        NYC.World.addObject(this, 0.0f); // approximation of the observer as static
+
+        // STARTING ROUTINE
+
         CarMonoBehaviour car;
 
-        foreach (GameObject car_obj in this.carsObj){
+        foreach (GameObject car_obj in this.cars){
 
             car = car_obj.GetComponent<CarMonoBehaviour>();
-            this.contractCar(car);
+            NYC.World.contractSpace(this, car);
 
         }
 
     }
 
-    void Update(){
-
-        // transform.Translate(this.velocity * Time.deltaTime, Space.World);
-        
-    }
-    */
 }
