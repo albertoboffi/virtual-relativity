f711c00cf1b8d57449d5716f2f29a696d58f2b88
Alberto Boffi <alberto2.boffi@mail.polimi.it>
2024-02-05 08:19:12 +0100

    feat: implement movement in space contraction probe scene

--- Assets/Scripts/space-contraction/probe/ParallelepipedMonoBehaviour.cs
+++ Assets/Scripts/space-contraction/probe/ParallelepipedMonoBehaviour.cs
@@ -3,6 +3,18 @@ using System.Collections.Generic;
 
 public class ParallelepipedMonoBehaviour : MonoBehaviour{
 
-    
+    void Start(){
+
+        // CONSTRUCTOR
+
+        SCProbe.World.addObject(this, 35.0f);
+
+    }
+
+    void Update(){
+
+        SCProbe.World.move(this);
+
+    }
 
 }
