93188c3b89599976d805e24161a51fed1b9afc0e
Alberto Boffi <alberto2.boffi@mail.polimi.it>
2024-02-05 08:50:35 +0100

    feat: adjust bounds of the parallelepiped movement

--- Assets/Scripts/space-contraction/probe/ParallelepipedMonoBehaviour.cs
+++ Assets/Scripts/space-contraction/probe/ParallelepipedMonoBehaviour.cs
@@ -3,17 +3,37 @@ using System.Collections.Generic;
 
 public class ParallelepipedMonoBehaviour : MonoBehaviour{
 
+    private float extr_pos; // position of the extreme of the journey, in absolute value
+
+    private void setStartingPosition(){
+
+        this.transform.position = new Vector3(-this.extr_pos, 0.6f, 5f);
+
+    }
+
     void Start(){
 
         // CONSTRUCTOR
 
         SCProbe.World.addObject(this, 35.0f);
 
+        this.extr_pos = 190f;
+
+        // STARTING ROUTINE
+
+        this.setStartingPosition();
+
     }
 
     void Update(){
 
         SCProbe.World.move(this);
+
+        if (this.transform.position.x >= this.extr_pos){
+
+            this.setStartingPosition();            
+
+        }
 
     }
 
