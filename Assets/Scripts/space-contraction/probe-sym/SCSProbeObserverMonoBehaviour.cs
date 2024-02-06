ae2213b449b18fb122beceee8a72bdedc8d988e6
Alberto Boffi <alberto2.boffi@mail.polimi.it>
2024-02-06 08:10:55 +0100

    feat: create SCSProbeObserverMonoBehaviour class

---
+++ Assets/Scripts/space-contraction/probe-sym/SCSProbeObserverMonoBehaviour.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SCSProbeObserverMonoBehavior : MonoBehaviour{
+
+    void Start(){
+
+        // CONSTRUCTOR
+
+        SCSProbe.World.addObject(this, 0.0f);
+
+    }
+
+    void Update(){
+
+            SCSProbe.World.contractSpace(this);
+
+    }
+
+}
