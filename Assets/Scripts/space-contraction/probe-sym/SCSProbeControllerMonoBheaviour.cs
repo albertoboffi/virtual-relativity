4287b8146d3bec4525b6e9477a998b19d491e7b5
Alberto Boffi <alberto2.boffi@mail.polimi.it>
2024-02-06 08:02:17 +0100

    feat: create SCSProbeControllerMonoBehaviour class

---
+++ Assets/Scripts/space-contraction/probe-sym/SCSProbeControllerMonoBheaviour.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Collections.Generic;
+
+public class SCSProbeControllerMonoBehaviour : MonoBehaviour{
+
+    public Slider speedLightSlider;
+    public Slider bulletTimeSlider;
+
+    public TextMeshProUGUI lightIndicator;
+    public TextMeshProUGUI timeIndicator;
+
+    private EventHandler eventHandler;
+
+    void Start(){
+
+        // CONSTRUCTOR
+
+        this.eventHandler = new EventHandler(SCSProbe.World);
+
+        this.eventHandler.speedLightInit(this.speedLightSlider, this.lightIndicator, 0.4f, 35.0f);
+
+        this.eventHandler.bulletTimeInit(this.bulletTimeSlider, this.timeIndicator);        
+
+    }
+
+    public void setSpeedLight(){
+
+        this.eventHandler.setSpeedLight(this.speedLightSlider, this.lightIndicator);
+
+    }
+
+    public void setBulletTime(){
+
+        this.eventHandler.setBulletTime(this.bulletTimeSlider, this.timeIndicator);
+
+    }
+
+}
