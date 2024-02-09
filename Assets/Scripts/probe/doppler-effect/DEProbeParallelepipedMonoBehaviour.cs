170b6d14e3c54a137de38effed68af1d308a5ef7
Alberto Boffi <alberto2.boffi@mail.polimi.it>
2024-02-09 08:56:20 +0100

    feat: create DEProbeParallelepipedMonoBehaviour class

---
+++ Assets/Scripts/probe/doppler-effect/DEProbeParallelepipedMonoBehaviour.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class DEProbeParallelepipedMonoBehaviour : MonoBehaviour{
+
+    private float extr_pos; // position of the extreme of the journey, in absolute value
+
+    private void setStartingPosition(){
+
+        this.transform.position = new Vector3(-this.extr_pos, 0.6f, 5f);
+
+    }
+
+    void Start(){
+
+        // CONSTRUCTOR
+
+        DEProbe.World.addObject(this, 35.0f, 513);
+
+        this.extr_pos = 190f;
+
+        // STARTING ROUTINE
+
+        this.setStartingPosition();
+
+    }
+
+    void Update(){
+
+        DEProbe.World.move(this);
+
+        if (this.transform.position.x >= this.extr_pos){
+
+            this.setStartingPosition();            
+
+        }
+
+    }
+
+}
