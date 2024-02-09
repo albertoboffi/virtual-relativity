cda1890a9243da3904813f3df43f9556ef4f6429
Alberto Boffi <alberto2.boffi@mail.polimi.it>
2024-02-09 21:07:09 +0100

    feat: implement infinite cars in nyc scene

--- Assets/Scripts/custom-app/space-contraction/nyc/NYCCarMonoBehaviour.cs
+++ Assets/Scripts/custom-app/space-contraction/nyc/NYCCarMonoBehaviour.cs
@@ -5,22 +5,18 @@ public class NYCCarMonoBehaviour : MonoBehaviour{
 
     public int ID;
 
-    void Start(){
+    private int cars_count;
+    private float cars_delay; // delay between each car
+    private float starting_time; // time of the car's start of the journey
 
-        // CONSTRUCTOR
-
-        NYC.World.addObject(this, 35.0f);
-
-        // STARTING ROUTINE
-
-        int cars_count = 7;
+    private void setStartingPosition(){
 
         float start_queue = 154.0f; // x-coordinate of the start of the queue
         float cars_distance = 9.5f; // distance between each car along the x-axis
 
         // cars are arranged in ascending order of their ID
 
-        float car_order = (cars_count - 1) - this.ID; 
+        float car_order = (this.cars_count - 1) - this.ID; 
         float x_pos = start_queue + cars_distance * car_order;
 
         // set starting position
@@ -29,14 +25,34 @@ public class NYCCarMonoBehaviour : MonoBehaviour{
 
     }
 
+    void Start(){
+
+        // CONSTRUCTOR
+
+        NYC.World.addObject(this, 35.0f);
+
+        this.cars_count = 7;
+        this.cars_delay = 5.0f; 
+        this.starting_time = this.cars_delay * this.ID; // cars start in order of their id
+
+        // STARTING ROUTINE
+
+        this.setStartingPosition();
+
+    }
+
     void Update(){
 
-        float cars_delay = 5.0f; // delay between each car
-        float starting_time = cars_delay * this.ID; // cars start in order of their id
-
-        if (NYC.World.wait(starting_time, this)){ // wait to start until it's your turn
+        if (NYC.World.wait(this.starting_time, this)){ // wait to start until it's your turn
 
             NYC.World.move(this);
+
+        }
+
+        if (this.transform.position.x >= 890f){ // the car reached the end of the journey
+
+            this.setStartingPosition();
+            this.starting_time += this.cars_delay * this.cars_count;
 
         }
 
