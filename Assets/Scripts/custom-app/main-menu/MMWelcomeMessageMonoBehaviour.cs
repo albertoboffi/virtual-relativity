b372119796336ac7809abd8fc539eaa14c2197c8
Alberto Boffi <alberto2.boffi@mail.polimi.it>
2024-02-10 20:19:23 +0100

    chore: change audio component

--- Assets/Scripts/custom-app/main-menu/MMWelcomeMessageMonoBehaviour.cs
+++ Assets/Scripts/custom-app/main-menu/MMWelcomeMessageMonoBehaviour.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MMSpaceshipMonoBehaviour : MonoBehaviour{
+public class MMWelcomeMessageMonoBehaviour : MonoBehaviour{
     
     private AudioSource audio;
 
