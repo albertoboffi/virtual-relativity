44f13bb989f73aa85ae46e5bdf91b5fd751c7d91
Alberto Boffi <alberto2.boffi@mail.polimi.it>
2024-02-10 22:24:46 +0100

    feat: initiate handle of teleportation ui

--- Assets/Scripts/custom-app/main-menu/MMControllerMonoBehaviour.cs
+++ Assets/Scripts/custom-app/main-menu/MMControllerMonoBehaviour.cs
@@ -1,14 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MMControllerMonoBehaviour : MonoBehaviour{
 
-    public GameObject startMenu;
+    public GameObject startMenu; // first step of the UI
+    public List<GameObject> previews; // second step of the UI
+
+    private void showPreview(GameObject preview){
+
+        preview.SetActive(true);
+
+    }
 
     public void play(){
 
-        Destroy(startMenu);
+        Destroy(startMenu); // hides the first step of the UI
+
+        foreach(GameObject preview in previews){ // shows the second step of the UI
+
+            this.showPreview(preview);
+
+        }
+
+    }
+
+    public void teleportToNYC(){
+
+        SceneManager.LoadScene("SpaceContraction_NYC");
 
     }
     
