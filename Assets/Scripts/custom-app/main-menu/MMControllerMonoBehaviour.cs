799d72fdfa55a040bac16ec32d69f31648738965
Alberto Boffi <alberto2.boffi@mail.polimi.it>
2024-02-22 13:15:08 +0100

    feat: add loading message when changing scene

--- Assets/Scripts/custom-app/main-menu/MMControllerMonoBehaviour.cs
+++ Assets/Scripts/custom-app/main-menu/MMControllerMonoBehaviour.cs
@@ -1,12 +1,17 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MMControllerMonoBehaviour : MonoBehaviour{
 
     public GameObject startMenu; // first step of the UI
     public List<GameObject> previews; // second step of the UI
+
+    public GameObject nycTeleportButton;
+    public GameObject seaTeleportButton;
 
     private static bool firstTime = true;
 
@@ -20,7 +25,7 @@ public class MMControllerMonoBehaviour : MonoBehaviour{
 
         Destroy(startMenu); // hides the first step of the UI
 
-        foreach(GameObject preview in previews){ // shows the second step of the UI
+        foreach(GameObject preview in this.previews){ // shows the second step of the UI
 
             this.showPreview(preview);
 
@@ -28,13 +33,28 @@ public class MMControllerMonoBehaviour : MonoBehaviour{
 
     }
 
+    private void setLoading(GameObject button_obj){
+
+        Button button = button_obj.GetComponent<Button>();
+
+        button.interactable = false; // disable the button
+
+        TextMeshProUGUI button_text = button.GetComponentInChildren<TextMeshProUGUI>();
+        button_text.text = "Loading...";
+
+    }
+
     public void teleportToNYC(){
+
+        this.setLoading(this.nycTeleportButton);
 
         SceneManager.LoadSceneAsync("SpaceContraction_NYC");
 
     }
 
     public void teleportToSea(){
+
+        this.setLoading(this.seaTeleportButton);
 
         SceneManager.LoadSceneAsync("TimeDilation_Sea");
 
