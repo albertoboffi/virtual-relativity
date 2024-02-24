b00835a07bb8eab0d343a1682dd2306cb9594480
Alberto Boffi <alberto2.boffi@mail.polimi.it>
2024-02-24 09:13:38 +0100

    feat: implement perpetual boats motion

--- Assets/Scripts/custom-app/doppler-effect/lake/LakeBoatMonoBehaviour.cs
+++ Assets/Scripts/custom-app/doppler-effect/lake/LakeBoatMonoBehaviour.cs
@@ -3,9 +3,25 @@ using System.Collections.Generic;
 
 public class LakeBoatMonoBehaviour : MonoBehaviour{
 
+    public int ID;
+
+    private int boats_count;
+    private float boats_delay; // delay between each boat
+    private float starting_time; // time of the boat's start of the journey
+
     private void setStartingPosition(){
 
-        this.transform.position = new Vector3(-200f, -0.938f, 22.5f);
+        float start_queue = -400f; // x-coordinate of the start of the queue
+        float boats_distance = 20f; // distance between each boat along the x-axis
+
+        // boats are arranged in ascending order of their ID
+
+        float boat_order = (this.boats_count - 1) - this.ID; 
+        float x_pos = start_queue + boats_distance * boat_order;
+
+        // set starting position
+
+        this.transform.position = new Vector3(x_pos, -1f, 22.5f);
 
     }
 
@@ -15,15 +31,32 @@ public class LakeBoatMonoBehaviour : MonoBehaviour{
 
         Lake.World.addObject(this, 35.0f, 500f);
 
+        this.boats_count = 2;
+        this.boats_delay = 10f; 
+        this.starting_time = this.boats_delay * this.ID; // boats start in order of their id
+
         // STARTING ROUTINE
+
+        this.setStartingPosition();
 
     }
 
     void Update(){
 
-        if (this.transform.position.x >= 200f){
+        // wait to start until it's your turn
+
+        if (!Lake.World.wait(this.starting_time, this)){
 
             this.setStartingPosition();
+
+        }
+
+        // the boat reached the end of the journey
+
+        if (this.transform.position.x >= 400f){ 
+
+            this.setStartingPosition();
+            this.starting_time += this.boats_delay * this.boats_count;
 
         }
 
