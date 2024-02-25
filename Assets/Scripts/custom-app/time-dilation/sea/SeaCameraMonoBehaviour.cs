4efc1dbed963c243bea62fc9209e7a27ca54200a
Alberto Boffi <alberto2.boffi@mail.polimi.it>
2024-02-25 13:03:39 +0100

    feat: create SeaCameraMonoBehaviour class

---
+++ Assets/Scripts/custom-app/time-dilation/sea/SeaCameraMonoBehaviour.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class SeaCameraMonoBehaviour : MonoBehaviour{
+
+    public Camera mainCamera;
+
+    private bool centered; // true if and only if the camera has been centered
+
+    void Start(){
+
+        this.centered = false;
+
+    }
+
+    private void centerCamera(){
+
+        // reset the orientation of the headset
+        
+        float camera_orientation = this.mainCamera.transform.rotation.eulerAngles.y;
+        
+        this.transform.Rotate(0, - camera_orientation, 0);
+
+        // reset the position of the headset
+
+        Vector3 camera_position = this.mainCamera.transform.position;
+
+        this.transform.position = new Vector3(
+
+            - camera_position.x,
+            this.transform.position.y,
+            - camera_position.z
+
+        );
+
+    }
+
+    void Update(){
+
+        if (! this.centered){
+
+            this.centerCamera();
+            this.centered = true;
+
+        }
+
+    }
+
+}
