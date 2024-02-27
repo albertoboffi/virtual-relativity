db0d620b6b441bc35383571967906c0be2839e75
Alberto Boffi <alberto2.boffi@mail.polimi.it>
2024-02-27 08:09:12 +0100

    feat: center camera at the beginning of each scene

--- Assets/Scripts/custom-app/space-contraction/nyc/NYCCameraMonoBehaviour.cs
+++ Assets/Scripts/custom-app/space-contraction/nyc/NYCCameraMonoBehaviour.cs
@@ -6,10 +6,12 @@ public class NYCCameraMonoBehaviour : MonoBehaviour{
     public Camera mainCamera;
 
     private bool centered; // true if and only if the camera has been centered
+    private Vector3 offset; // amount of space the camera is shifted
 
     void Start(){
 
         this.centered = false;
+        this.offset = new Vector3(0, 0, 0);
 
     }
 
@@ -20,6 +22,26 @@ public class NYCCameraMonoBehaviour : MonoBehaviour{
         float camera_orientation = this.mainCamera.transform.rotation.eulerAngles.y;
         
         this.transform.Rotate(0, - camera_orientation, 0);
+
+        // reset the position of the camera
+
+        Vector3 camera_position = this.mainCamera.transform.position;
+
+        this.offset = new Vector3(
+
+            camera_position.x - 530f,
+            0f,
+            camera_position.z + 63f
+
+        );
+
+        this.transform.position = new Vector3(
+
+            this.transform.position.x - this.offset.x,
+            this.transform.position.y,
+            this.transform.position.z - this.offset.z
+
+        );
 
     }
 
@@ -34,5 +56,10 @@ public class NYCCameraMonoBehaviour : MonoBehaviour{
 
     }
 
+    public Vector3 getOffset(){
+
+        return this.offset;
+
+    }
 
 }
--- Assets/Scripts/custom-app/time-dilation/sea/SeaCameraMonoBehaviour.cs
+++ Assets/Scripts/custom-app/time-dilation/sea/SeaCameraMonoBehaviour.cs
@@ -6,10 +6,12 @@ public class SeaCameraMonoBehaviour : MonoBehaviour{
     public Camera mainCamera;
 
     private bool centered; // true if and only if the camera has been centered
+    private Vector3 offset; // amount of space the camera is shifted
 
     void Start(){
 
         this.centered = false;
+        this.offset = new Vector3(0, 0, 0);
 
     }
 
@@ -21,6 +23,26 @@ public class SeaCameraMonoBehaviour : MonoBehaviour{
         
         this.transform.Rotate(0, - camera_orientation, 0);
 
+        // reset the position of the camera
+
+        Vector3 camera_position = this.mainCamera.transform.position;
+
+        this.offset = new Vector3(
+
+            camera_position.x,
+            0f,
+            camera_position.z
+
+        );
+
+        this.transform.position = new Vector3(
+
+            this.transform.position.x - this.offset.x,
+            this.transform.position.y,
+            this.transform.position.z - this.offset.z
+
+        );
+
     }
 
     void Update(){
@@ -31,6 +53,12 @@ public class SeaCameraMonoBehaviour : MonoBehaviour{
             this.centered = true;
 
         }
+
+    }
+
+    public Vector3 getOffset(){
+
+        return this.offset;
 
     }
 
